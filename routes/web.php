6ef14dac04f8ab23718e52db139fe82b204047a2
Roger Lopez <roger.lopez@zarpo.com.br>
2021-07-26 14:03:03 -0300

    Sec15 testbrabor6

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -48,6 +49,8 @@ Route::get('authors/{author:username}' , function (User $author){
       'categories' => Category::all()
     ]);
 }); 
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
   // Find a post by its slug and pass it to a view  
 
   /*  
